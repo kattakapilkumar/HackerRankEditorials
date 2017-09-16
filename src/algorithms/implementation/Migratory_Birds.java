e7548527ed0c4224bc057557d1c2f71320cbec60
kattakapilkumar <kapilkatta123@gmail.com>
2017-09-16 13:51:08 +0530

    Migratory_Birds

--- src/algorithms/implementation/Migratory_Birds.java
+++ src/algorithms/implementation/Migratory_Birds.java
@@ -1,9 +1,28 @@
 package algorithms.implementation;
 
+import java.util.*;
 public class Migratory_Birds {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] types = new int[n];
+        for(int arr_i=0; arr_i < n; arr_i++){
+            types[arr_i] = in.nextInt();
+        }
+        int[] frequencies = new int[6]; //A
+        for (int i = 0; i < types.length; i++) { //B
+            frequencies[types[i]] += 1; //C
+        }
+        int mostCommon = 0;
+        for (int i = 1; i < frequencies.length; i++) { //D
+            if (frequencies[mostCommon] < frequencies[i]) {
+                mostCommon = i; //E
+            }
+        }
+        System.out.println(mostCommon);
 
 	}
 
